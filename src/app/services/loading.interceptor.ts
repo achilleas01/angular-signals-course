65db1105c9aab7f9f395b2578fb5edcbc95650f0
Achilles Katsaros <achilleas01@yahoo.gr>
2024-09-07 17:32:16 +0300

    chore: create a loading interceptor

---
+++ src/app/services/loading.interceptor.ts
@@ -0,0 +1,17 @@
+import { HttpHandlerFn, HttpInterceptorFn, HttpRequest } from "@angular/common/http";
+import { inject } from "@angular/core";
+import { LoadingService } from "../loading/loading.service";
+import { finalize } from "rxjs";
+
+export const loadingInterceptor: HttpInterceptorFn =
+(req: HttpRequest<unknown>, next: HttpHandlerFn) => {
+
+    const loadingService = inject(LoadingService);
+    return next(req)
+      .pipe(
+        finalize(() => {
+            loadingService.loadingOff();
+        })
+      )
+
+}
